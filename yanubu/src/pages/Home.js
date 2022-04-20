df3a9a7348b2feef94954cbefe971592059287c8
fajar098123 <fajar098123@gmail.com>
2022-04-20 12:23:14 +0700

    update

--- yanubu/src/pages/Home.js
+++ yanubu/src/pages/Home.js
@@ -1,3 +1,11 @@
+function importAll(r) {
+  let img = {};
+  r.keys().map(item => { img[item.replace('./', '')] = r(item); });
+  return img;
+}
+
+const img = importAll(require.context('../img', false, /\.png/));
+
 function Home() {
   return (
     <div className="App">
@@ -5,51 +13,85 @@ function Home() {
       <section id="hero" class="d-flex align-items-center">
 
         <div class="container">
-            <div class="d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
-              <h1>Selamat Datang</h1>
-              <h2 class="col-lg-5 d-flex flex-column">di Website <span>Yayasan Nurul Burhan</span> Bondowoso</h2>
-              <div class="d-flex justify-content-center">
-                <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-sosmed"><i class="bx bxl-instagram-alt"></i><span>Watch Video</span></a>
-                <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-sosmed"><i class='bx bxl-youtube'></i><span>Watch Video</span></a>
-                <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-sosmed"><i class='bx bxl-facebook-square'></i><span>Watch Video</span></a>
-              </div>
+          <div class="d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+            <h1>Selamat Datang</h1>
+            <h2 class="col-lg-5 d-flex flex-column">di Website <span>Yayasan Nurul Burhan</span> Bondowoso</h2>
+            <div class="d-flex justify-content-center">
+              <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-sosmed"><i class="bx bxl-instagram-alt"></i><span>yayasan_nurul_burhan</span></a>
+              <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-sosmed"><i class='bx bxl-youtube'></i><span>Yayasan Nurul Burhan</span></a>
+              <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-sosmed"><i class='bx bxl-facebook-square'></i><span>Yayasan Nurul Burhan</span></a>
             </div>
+          </div>
         </div>
 
       </section>
       {/* <!-- End Hero --> */}
 
-      <section id="about" class="about">
-      <div class="container" data-aos="fade-up">
+      <section class="wrapper">
+        <div class="container">
 
-        <div class="section-title">
-          <h2>About Us</h2>
-        </div>
-
-        <div class="row content">
-          <div class="col-lg-6">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
-            </p>
-            <ul>
-              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
-              <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit</li>
-              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
-            </ul>
+          <div class="row">
+            <div class="col-md-3 pb-2">
+              <div class="card text-white card-has-bg click-col  shadow" style={{ backgroundImage: `url(${img['img1.png']})` }}>
+                <div class="card-img-overlay d-flex flex-column">
+                  <div class="card-body">
+                    <h4 class="card-title mt-0 "><a class="text-white" herf="#">PPI PUTRA - NURUL BURHAN</a></h4>
+                    <p class="card-meta mb-2">Untuk Info lebih lengkap terkait PPI PUTRA - NURUL BURHAN dapat di lihat di sini</p>
+                    {/* <small><i class="far fa-clock"></i> October 15, 2020</small> */}
+                    <a class="" href="#"><h5 class="d-inline">Baca Selengkapnya</h5><i class='bx bxs-chevron-right bx-xs'></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 pb-2">
+              <div class="card text-white card-has-bg click-col shadow" style={{ backgroundImage: `url(${img['img2.png']})` }}>
+                <div class="card-img-overlay d-flex flex-column">
+                  <div class="card-body">
+                    <h4 class="card-title mt-0 "><a class="text-white" herf="#">PPI PUTRA - NURUL BURHAN</a></h4>
+                    <p class="card-meta mb-2">Untuk Info lebih lengkap terkait PPI PUTRA - NURUL BURHAN dapat di lihat di sini</p>
+                    {/* <small><i class="far fa-clock"></i> October 15, 2020</small> */}
+                    <a class="" href="#"><h5 class="d-inline">Baca Selengkapnya</h5><i class='bx bxs-chevron-right bx-xs'></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 pb-2">
+              <div class="card text-white card-has-bg click-col shadow" style={{ backgroundImage: `url(${img['img3.png']})` }}>
+                <div class="card-img-overlay d-flex flex-column">
+                  <div class="card-body">
+                    <h4 class="card-title mt-0 "><a class="text-white" herf="#">PPI PUTRA - NURUL BURHAN</a></h4>
+                    <p class="card-meta mb-2">Untuk Info lebih lengkap terkait PPI PUTRA - NURUL BURHAN dapat di lihat di sini</p>
+                    {/* <small><i class="far fa-clock"></i> October 15, 2020</small> */}
+                    <a class="" href="#"><h5 class="d-inline">Baca Selengkapnya</h5><i class='bx bxs-chevron-right bx-xs'></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 pb-2">
+              <div class="card text-white card-has-bg click-col shadow" style={{ backgroundImage: `url(${img['img4.png']})` }}>
+                <div class="card-img-overlay d-flex flex-column">
+                  <div class="card-body">
+                    <h4 class="card-title mt-0 "><a class="text-white" herf="#">PPI PUTRA - NURUL BURHAN</a></h4>
+                    <p class="card-meta mb-2">Untuk Info lebih lengkap terkait PPI PUTRA - NURUL BURHAN dapat di lihat di sini</p>
+                    {/* <small><i class="far fa-clock"></i> October 15, 2020</small> */}
+                    <a class="" href="#"><h5 class="d-inline">Baca Selengkapnya</h5><i class='bx bxs-chevron-right bx-xs'></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div class="col-lg-6 pt-4 pt-lg-0">
-            <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <a href="#" class="btn-learn-more">Learn More</a>
+        </div>
+      </section>
+
+      <section class="artikel">
+        <div class="container">
+          <div class="d-flex justify-content-between">
+            <div class="title-section"><h1>ARTIKEL</h1></div>
+            <div class="read-more d-flex align-items-center"><a class="btn-read-more" href="#"><h5 class="d-inline">Baca Selengkapnya</h5><i class='bx bxs-chevron-right bx-xs'></i></a></div>
           </div>
         </div>
+      </section>
 
-      </div>
-    </section>
 
     </div>
   );
